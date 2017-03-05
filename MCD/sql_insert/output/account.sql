c08a766c09a244264d69391ad88f7c9af010ad29
cdcvidal <cdcvidal@yahoo.fr>
2017-03-05 11:35:59 +0100

    ADD MCD and INSERT Data SQL

---
+++ MCD/sql_insert/output/account.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.accounts(
+	id, login, password)
+	VALUES (1, 'user1@mymail.org', '123456');
---
+++ MCD/sql_insert/output/type.sql
@@ -0,0 +1,27 @@
+INSERT INTO public.types(
+	id, name, size)
+	VALUES (1, 'VARCHAR', 250);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (2, 'INT', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (3, 'FLOAT', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (4, 'DATE', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (5, 'BOOL', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (6, 'JSON', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (7, 'NUMERIC', NULL);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (8, 'CHAR', 25);
+INSERT INTO public.types(
+		id, name, size)
+		VALUES (9, 'TEXT', NULL);
