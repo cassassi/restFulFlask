39ff2257179d289938b75a783d5f73da521ac170
SThomaty <sandiethomaty@gmail.com>
2017-03-10 00:40:32 +0100

    addition of missing classes and implementation of get/contributions and get/contributions/poi (plus beginning of get/versions/poi (not finished))

--- MCD/sql_insert/output/users.sql
+++ MCD/sql_insert/output/users.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.users(
-	id, lastname, firstname, email, picture, idcategory, idaccount)
+	id, lastname, firstname, email, picture, categories_id, accounts_id)
 	VALUES (1, 'Tom', 'Trucani', 'user1@mymail.org', '', 3, 1);
--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 
-
 class Contributions(db.Model):
     version = db.Column(db.Integer)
     status = db.Column(db.String(35))
@@ -32,6 +31,31 @@ class Contributions(db.Model):
 
     def __repr__(self):
         return '<Contributions {}>'.format(self.pois.id + " " + self.fields.name + " " + self.values.value)
+
+class Comments(db.Model):
+    message = db.Column(db.Text)
+    title = db.Column(db.String(50))
+    createdDate = db.Column(db.Date)
+    iduser = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True)
+    idpoi = db.Column(db.Integer, db.ForeignKey(
+        'pois.id'), primary_key=True)
+
+    users = db.relationship("Users", backref=db.backref(
+        "comments", cascade="all, delete-orphan"))
+    pois = db.relationship("Pois", backref=db.backref(
+        "comments", cascade="all, delete-orphan"))
+
+class Rewards(db.Model):
+    idaward = db.Column(db.Integer, db.ForeignKey(
+        'awards.id'), primary_key=True)
+    iduser = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True)
+
+    award = db.relationship("Awards", backref=db.backref(
+        "rewards", cascade="all, delete-orphan"))
+    users = db.relationship("Users", backref=db.backref(
+        "rewards", cascade="all, delete-orphan"))
 
 
 class Generaltypes(db.Model):
@@ -100,8 +124,29 @@ class Users(db.Model):
     email = db.Column(db.String(35))
     picture = db.Column(db.String(35))
     values = db.relationship('Values', backref='users', lazy='dynamic')
+    categories_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    accounts_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
 
-# poi
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    users = db.relationship('Users', backref='categories', lazy='dynamic')
+
+class Accounts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    users = db.relationship('Users', backref='accounts', lazy='dynamic')
+
+class Awards(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    typeAward = db.Column(db.String(50), nullable=False)
+    label = db.Column(db.String(25), nullable=False)
+
+
+
+# --------------------------------------------pois---------------------------------------------------------
 @app.route('/api/pois', methods=['GET'])
 def returnAllPois():
 	allAsso = Contributions.query.all()
@@ -202,9 +247,153 @@ def addOnePoi():
             db.session.commit()
     return jsonify({'Poi' : currentPoi.idPoi}), 201
 
+# ---------------------------------------contributions---------------------------------------------------------
+@app.route('/api/contributions', methods=['GET'])
+def returnAllContributions():
+    allAsso = Contributions.query.all()
+
+    malist = [] #format qui nous arrange pas
+    tempPoi=0
+    tempField=0
+    tempValue=0
+    for ass in allAsso:
+
+        oneValue = Values.query.filter_by(id=ass.idvalue).first()
+        if(tempValue!=oneValue):
+            tempValue=oneValue
+            malist.append({'id': oneValue.id, 'created_date': oneValue.createddate, 'value': oneValue.value})
+
+            oneUser = Values.query.filter_by(id=oneValue.users_id).first()
+            theUser = Users.query.filter_by(id=oneUser.id).first()
+            malist.append({'user_name': theUser.lastname})
+
+        onePoi = Pois.query.filter_by(id=ass.idpoi).first()
+        malist.append({'poi_id': onePoi.id})
+
+        oneField = Fields.query.filter_by(id=ass.idfield).first()
+        if(tempField!=oneField):
+            tempField=oneField
+            malist.append({'field.name' : oneField.name})
+
+        malist.append({'status': ass.status})
+    #creation d'une format approprier en utilisant le dictionnaire
+    malistFormatBon=[] #format ideal
+    dictionnaire={}
+    compteurContrib=0;
+    compteur=2
+    for i in range(len(malist)):
+        for cle, valeur in malist[i].items():
+            if(cle=='id'):
+                compteurContrib+=1;
+            if(compteur==compteurContrib):
+                malistFormatBon.append(dictionnaire)
+                dictionnaire={}
+                compteur+=1
+            dictionnaire[cle]=valeur
+    malistFormatBon.append(dictionnaire)
+    return jsonify({'contributions': malistFormatBon})
+
+
+@app.route('/api/contributions/<int:idp>', methods=['GET'])
+def returnContributionsById(idp):
+    allAsso = Contributions.query.filter_by(idpoi=idp).all()
+
+    malist = [] #format qui nous arrange pas
+    tempPoi=0
+    tempField=0
+    tempValue=0
+    for ass in allAsso:
+
+        oneValue = Values.query.filter_by(id=ass.idvalue).first()
+        if(tempValue!=oneValue):
+            tempValue=oneValue
+            malist.append({'id': oneValue.id, 'created_date': oneValue.createddate, 'value': oneValue.value})
+
+            oneUser = Values.query.filter_by(id=oneValue.users_id).first()
+            theUser = Users.query.filter_by(id=oneUser.id).first()
+            malist.append({'user_name': theUser.lastname})
+
+        onePoi = Pois.query.filter_by(id=ass.idpoi).first()
+        malist.append({'poi_id': onePoi.id})
+
+        oneField = Fields.query.filter_by(id=ass.idfield).first()
+        if(tempField!=oneField):
+            tempField=oneField
+            malist.append({'field.name' : oneField.name})
+
+        malist.append({'status': ass.status})
+    #creation d'une format approprier en utilisant le dictionnaire
+    malistFormatBon=[] #format ideal
+    dictionnaire={}
+    compteurContrib=0;
+    compteur=2
+    for i in range(len(malist)):
+        for cle, valeur in malist[i].items():
+            if(cle=='id'):
+                compteurContrib+=1;
+            if(compteur==compteurContrib):
+                malistFormatBon.append(dictionnaire)
+                dictionnaire={}
+                compteur+=1
+            dictionnaire[cle]=valeur
+    malistFormatBon.append(dictionnaire)
+    return jsonify({'contributionsById': malistFormatBon})
+
+# --------------------------------------------versions---------------------------------------------------------
+
+@app.route('/api/versions/poi/<int:idp>', methods=['GET'])
+def returnVersionsOfOnePoi(idp):
+    allAsso = Contributions.query.filter_by(idpoi=idp).all()
+
+    malist = [] #format qui nous arrange pas
+    tempPoi=0
+    tempField=0
+    tempValue=0
+    tempVersion = 0
+    for ass in allAsso:
+
+        oneVersion = Contributions.query.filter_by(version=ass.version).first()
+        #if(tempVersion!=oneVersion):
+            #tempVersion=oneVersion
+        malist.append({'version': oneVersion.version})
+
+        onePoi = Pois.query.filter_by(id=ass.idpoi).first()
+        #if(tempPoi!=onePoi):
+            #tempPoi=onePoi
+        malist.append({'id': onePoi.id, 'tour_id': onePoi.tour_id})
+
+        oneField = Fields.query.filter_by(id=ass.idfield).first()
+        if(tempField!=oneField):
+            tempField=oneField
+            oneValue = Values.query.filter_by(id=ass.idvalue).first()
+            if(tempValue!=oneValue):
+                tempValue=oneValue
+                malist.append({oneField.name : oneValue.value})
+
+
+
+    #creation d'une format approprier en utilisant le dictionnaire
+    malistFormatBon=[] #format ideal
+    dictionnaire={}
+    compteurVersion=0;
+    compteur=2
+    for i in range(len(malist)):
+        for cle, valeur in malist[i].items():
+            if(cle=='version'):
+                compteurVersion+=1;
+            if(compteur==compteurVersion):
+                malistFormatBon.append(dictionnaire)
+                dictionnaire={}
+                compteur+=1
+            dictionnaire[cle]=valeur
+    malistFormatBon.append(dictionnaire)
+    return jsonify({'versions': malistFormatBon})
+
 
 db.create_all()
 db.session.commit()
+
+
 
 
 if __name__ == "__main__":
